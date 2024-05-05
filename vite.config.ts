df2a45b6f6f91be35286ee231069d6b9dd730817
Ryland Shaw <ryland.shaw@gmail.com>
2024-05-05 15:58:05 -0400

    adds base field

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  htmlPrerender({
+  base: '/rylandshaw.com/',
+  plugins: [react(), htmlPrerender({
     staticDir: path.join(__dirname, "dist"),
     routes: ["/"],
   }),],
-  
+
 })
