a881dbe90fa08ba4e42f9b988b8547046132119c
Ryland Shaw <ryland.shaw@gmail.com>
2024-05-05 16:56:55 -0400

    general layout

--- src/Site/Site.tsx
+++ src/Site/Site.tsx
@@ -1,8 +1,49 @@
-import './Site.css'
+import './Typography.scss'
+import './Layout.scss'
 
 export default function Site() {
 
   return (
-    <h1>Hello, world!</h1>
+    <div className='Site'>
+      <header>
+        <h1>Ryland Shaw</h1>
+      </header>
+      <div className='Bio'>
+        <section className='Profile'>
+          <img className='ProfilePicture' />
+          <nav>
+            <a href="mailto:ryland.shaw@gmail.com">ryland.shaw@gmail.com</a>
+            <a href="/">Resume</a>
+            <a href="/">LinkedIn</a>
+          </nav>
+        </section>
+        <section className='Description'>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+          <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie </p>
+        </section>
+
+      </div>
+      <section className='News'>
+        <section className='BigNews'>
+          <h2>Big News</h2>
+          <article>
+            Big news 1 / Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+          </article>
+          <article>
+            Big news 2 / Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+          </article>
+        </section>
+        <section className='SmallNews'>
+          <h2>Small News</h2>
+          <article>
+            Small news 1 / Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+          </article>
+          <article>
+            Small news 2 / Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+          </article>
+        </section>
+      </section>
+    </div>
   )
 }
