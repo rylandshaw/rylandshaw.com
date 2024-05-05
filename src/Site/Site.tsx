2732542f5d9825a0ab8f973137fea1bf833f8f02
Ryland Shaw <ryland.shaw@gmail.com>
2024-05-05 16:05:33 -0400

    updates styling

---
+++ src/Site/Site.tsx
@@ -0,0 +1,8 @@
+import './Site.css'
+
+export default function Site() {
+
+  return (
+    <h1>Hello, world!</h1>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Site from './Site/Site.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Site />
   </React.StrictMode>,
 )
