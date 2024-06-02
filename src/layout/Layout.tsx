ad20845b943b3e91bd19a6732cd17d609d7e90d0
Ryland Shaw <ryland.shaw@gmail.com>
2024-06-02 11:09:33 -0400

    updates header links

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,35 +3,18 @@ import '../styles/Layout.scss'
 import Description from '../sections/Description'
 import BigNews from '../sections/BigNews'
 import LittleNews from '../sections/LittleNews'
-import profile from '../assets/profile.jpeg'
-import penguin from '../assets/swim penguin.svg'
+import Header from '../sections/Header'
 
 export default function Layout() {
 
   return (
     <div className='Site'>
       <header className='Header'>
-        <div className="Profile">
-          <img src={profile} />
-          <div>
-            <h1>Ryland Shaw</h1>
-            <nav>
-              <a href="/">CV</a>
-              <span>❄</span>
-              <a href="/">Scholar</a>
-              <span>❄</span>
-              <a href="/">LinkedIn</a>
-              <a className='EmailLink' href="mailto:ryland.shaw@gmail.com">ryland.shaw@gmail.com</a>
-            </nav>
-          </div>
-        </div>
-        <img className="Swim" src={penguin} />
+        <Header />
       </header>
-
       <section className='Description'>
         <Description />
       </section>
-
       <section className='News'>
         <section className='BigNews'>
           <BigNews />
---
+++ src/sections/Header.tsx
@@ -0,0 +1,23 @@
+import profile from '../assets/profile.jpeg'
+import penguin from '../assets/swim penguin.svg'
+
+export default function Header() {
+  return (
+    <>
+      <div className="Profile">
+        <img src={profile} />
+        <div>
+          <h1>Ryland Shaw</h1>
+          <nav>
+            <a href="https://docs.google.com/document/d/1-EX4dSu5AAB0UC_YaHiMXa-ddoPEF6z7oB6f7sqPb7s/view" target="_blank">CV</a>
+            <span>❄</span>
+            <a href="https://scholar.google.com/citations?user=fHPG_O0AAAAJ" target="_blank">Scholar</a>
+            <span>❄</span>
+            <a href="https://www.linkedin.com/in/ryland-shaw/" target="_blank">LinkedIn</a>
+            <span className='EmailLink'>v-rylandshaw@microsoft.com</span>
+          </nav>
+        </div>
+      </div>
+      <img className="Swim" src={penguin} /></>
+  )
+}
