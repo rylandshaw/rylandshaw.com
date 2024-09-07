8f97134fdd6b42c841b60c8244bf47a39e277d4b
Ryland Shaw <ryland.shaw@gmail.com>
2024-09-07 13:35:49 -0400

    Updated description text and news texts

--- src/sections/BigNews.tsx
+++ src/sections/BigNews.tsx
@@ -8,7 +8,7 @@ export default function BigNews() {
                 <h2>Big News!</h2>
             </header>
             <article>
-                Apr 2024: The feature-length documentary I produced and assistant edited, <em><a href="https://amakki.net" target="_blank">Amakki</a></em>, premiered at the 2024 Atlanta Film Festival, where it won best documentary.
+                Apr 2024: The documentary I produced and assistant edited, <em><a href="https://amakki.net" target="_blank">Amakki</a></em>, premiered at the 2024 Atlanta Film Festival, where it won Best Documentary Feature.
             </article>
             <article>
                 Dec 2023: I successfully defended my master’s thesis, <em>The Logic of Imitation and the Reconfiguration of News in Climate Communication on TikTok</em>.
@@ -17,7 +17,7 @@ export default function BigNews() {
                 June 2023: I joined Microsoft Research’s Social Media Collective as a pre-doctoral research assistant.
             </article>
             <article>
-                October 2022: I co-authored a paper with Professor Tammara Soma, <em>To the Farm, Mars and Beyond: Technologies for Growing Food in Space, the Future of Long-Duration Space Missions, and Earth Implication in English News Media Coverage</em>, published in Frontiers in Communication.
+                October 2022: I co-authored a paper with Professor Tammara Soma, <em><a href="https://www.frontiersin.org/journals/communication/articles/10.3389/fcomm.2022.1007567/full" target="_blank">To the Farm, Mars and Beyond: Technologies for Growing Food in Space, the Future of Long-Duration Space Missions, and Earth Implication in English News Media Coverage</a></em>, published in Frontiers in Communication.
             </article>
         </>
     )
--- src/sections/LittleNews.tsx
+++ src/sections/LittleNews.tsx
@@ -8,13 +8,16 @@ export default function LittleNews() {
                 <h2>Little News</h2>
             </header>
             <article>
+                October 2024: Nancy Baym, Chuncheng Liu and I had our upcoming paper accepted to AoIR, <em>"It’s a Friend! It’s a Puppy! It’s AI!: Making Sense of Copilot"</em>. See you in Sheffield!
+            </article>
+            <article>
+                November 2024: I am organizing a hybrid workshop at CSCW in Costa Rica on the <em><a href="https://sites.google.com/view/thehumanfactorinairedteaming/home?authuser=0" target="_blank">Human Factor in AI Red Teaming</a></em>, alongside a team of researchers from Microsoft Research, Carnegie Mellon, UI Chicago and U of Michigan.
+            </article>
+            <article>
                 June 2024: A book chapter I wrote with Shane Gunster and Robert Neubauer, <em>Sourcing Matters: Activism, expertise and alternative media</em>, was accepted and will appear in Anabela Carvalho and Tarla Rai Peterson’s <em>Handbook on Environmental Communications</em>.
             </article>
             <article>
                 March 2024: I was credited for research assistance on three recently published papers: Tarleton Gillespie, <em>Generative AI and the Politics of Visibility</em>; Janet A. Vertesi and danah boyd, <em>The Resource Bind: System Failure and Legitimacy Threats in Sociotechnical Organizations</em>; and Maria P. Angel and danah boyd, <em>Techno-legal Solutionism: Regulating Children’s Online Safety in the United States</em>.
-            </article>
-            <article>
-                October 2023: I presented preliminary findings of my thesis at the Association of Internet Researchers conference in Philadelphia. The presentation was also accepted, but not presented at IAMCR23 and the Canadian Communication Association conferences.
             </article>
         </>
     )
