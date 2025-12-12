f721c9df7fc916d6814ef8f91e07651801ecede0
Ryland Shaw <ryland.shaw@gmail.com>
2025-12-12 11:14:34 -0800

    updated news sections

--- src/sections/BigNews.tsx
+++ src/sections/BigNews.tsx
@@ -8,13 +8,16 @@ export default function BigNews() {
                 <h2>Big News!</h2>
             </header>
             <article>
+                Dec 2025: I published a new article in the International Journal of Communication: <em><a href="https://ijoc.org/index.php/ijoc/article/view/24604" target="_blank">Is TikTok “for” News? Sociotechnical Frictions and Workarounds in Environmental News Events on TikTok</a></em>.
+            </article>
+            <article>
                 Aug 2025: I've joined USC's doctoral program in Communication as an Annenberg Fellow! See my profile <em><a href="https://annenberg.usc.edu/phd-students/ryland-shaw" target="_blank">here</a></em>. 
             </article>
             <article>
                 Apr 2024: The documentary I produced and assistant edited, <em><a href="https://amakki.net" target="_blank">Amakki</a></em>, premiered at the 2024 Atlanta Film Festival, where it won Best Documentary Feature. <em>Amakki</em> was also an official selection of FESPACO 2025, 2024 Black Harvest Film Festival in Chicago and the 2024 International Images Film Festival for Women in Zimbabwe. 
             </article>
             <article>
-                Dec 2023: I successfully defended my master’s thesis, <em>The Logic of Imitation and the Reconfiguration of News in Climate Communication on TikTok</em>. I've since adapted it into a paper under consideration at the International Journal of Communication.
+                Dec 2023: I successfully defended my master’s thesis, <em>The Logic of Imitation and the Reconfiguration of News in Climate Communication on TikTok</em>, finishing my time at Simon Fraser University.
             </article>
             <article>
                 June 2023: I joined Microsoft Research’s Social Media Collective as a pre-doctoral research assistant.
--- src/sections/LittleNews.tsx
+++ src/sections/LittleNews.tsx
@@ -8,7 +8,7 @@ export default function LittleNews() {
                 <h2>Little News</h2>
             </header>
             <article>
-                Aug 2025: Two pre-prints out!: <em><a href="https://arxiv.org/abs/2505.24246" target="_blank">Locating Risk: Task Designers and the Challenge of Risk Disclosure in RAI Content Work</a></em>, submitted to CSCW, and <em><a href="https://arxiv.org/pdf/2509.12140" target="_blank">Worker Discretion Advised: Co-designing Risk Disclosure in Crowdsourced
+                Aug 2025: Two pre-prints out!: <em><a href="https://arxiv.org/abs/2505.24246" target="_blank">Locating Risk: Task Designers and the Challenge of Risk Disclosure in RAI Content Work</a></em>, accepted to CSCW 2026, and <em><a href="https://arxiv.org/pdf/2509.12140" target="_blank">Worker Discretion Advised: Co-designing Risk Disclosure in Crowdsourced
 Responsible AI (RAI) Content Work</a></em>, submitted to CHI. These projects were led by Alice Q. Zhang, and supported by myself, Ziqi Yang, Laura Dabbish, Jina Suh, and Hong Shen.  
             </article>
             <article>
