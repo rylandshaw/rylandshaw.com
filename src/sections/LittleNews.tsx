a39b84f881c4b2367281bca9b05347a04e7790ee
Ryland Shaw <ryland.shaw@gmail.com>
2026-04-20 17:56:27 -0700

    typo fix

--- src/sections/LittleNews.tsx
+++ src/sections/LittleNews.tsx
@@ -8,8 +8,7 @@ export default function LittleNews() {
                 <h2>Little News</h2>
             </header>
             <article>
-                Aug 2025: Two papers coming out soon: <em><a href="https://arxiv.org/abs/2505.24246" target="_blank">Locating Risk: Task Designers and the Challenge of Risk Disclosure in RAI Content Work</a></em>, accepted to CSCW 2026, and <em><a href="https://dl.acm.org/doi/full/10.1145/3772318.3791558" target="_blank">Worker Discretion Advised: Co-designing Risk Disclosure in Crowdsourced, which we presented at CHI2026 in Barcelona.
-Responsible AI (RAI) Content Work</a></em>, accepted to CHI 2026. These projects were led by Alice Q. Zhang, and supported by myself, Ziqi Yang, Laura Dabbish, Jina Suh, and Hong Shen.  
+                Aug 2025: Two papers coming out soon: <em><a href="https://arxiv.org/abs/2505.24246" target="_blank">Locating Risk: Task Designers and the Challenge of Risk Disclosure in RAI Content Work</a></em>, accepted to CSCW 2026, and <em><a href="https://dl.acm.org/doi/full/10.1145/3772318.3791558" target="_blank">Worker Discretion Advised: Co-designing Risk Disclosure in Crowdsourced</a></em>, which we presented at CHI2026 in Barcelona. These projects were led by Alice Q. Zhang, and supported by myself, Ziqi Yang, Laura Dabbish, Jina Suh, Mary Gray and Hong Shen.  
             </article>
             <article>
                 April 2025: I gave a talk titled <em>From questioning energy to questioning power: Deconstructing 'AI for Sustainability'</em> to members of Seton Hall University's Teaching, Learning, and Technology Roundtable AI subcommittee.
